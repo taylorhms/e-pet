c4397823c30791851909f66f189593f5eb7e42a9
taylorhms <taylorhenriquems@gmail.com>
2023-12-27 00:42:33 -0300

    fix: url api local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     api: {
-        url: 'http://192.168.0.104:8080/epet-api'
+        url: 'http://localhost:8080/epet-api'
     },
     userStorageKey: 'username'
 };
